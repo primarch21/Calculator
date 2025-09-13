33f561c82b26077806c577e43f07c51678381426
primarch21 <amidnauk@gmail.com>
2025-09-13 17:08:55 +0300

    Реализован калькулятор

---
+++ rpn_calculator/include/rpn.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stack>
+#include <string>
+
+double evaluate_rpn(const std::string& expression);
---
+++ rpn_calculator/tests/test_rpn.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+#include "rpn.h"
+#include <stdexcept>
