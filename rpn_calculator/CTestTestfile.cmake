690a2dca5305b477b6aaa9bfee2ac9845b97c34f
primarch21 <amidnauk@gmail.com>
2025-09-17 20:30:55 +0300

    Пройдены тесты

---
+++ rpn_calculator/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/primarch21/Calculator/rpn_calculator
+# Build directory: /home/primarch21/Calculator/rpn_calculator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[rpn_tests]=] "/home/primarch21/Calculator/rpn_calculator/rpn_tests")
+set_tests_properties([=[rpn_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/primarch21/Calculator/rpn_calculator/CMakeLists.txt;39;add_test;/home/primarch21/Calculator/rpn_calculator/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
---
+++ rpn_calculator/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/primarch21/Calculator/rpn_calculator/_deps/googletest-src/googletest
+# Build directory: /home/primarch21/Calculator/rpn_calculator/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
